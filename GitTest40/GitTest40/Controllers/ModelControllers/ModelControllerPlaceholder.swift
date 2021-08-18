07617b3e41c4e2800dcde007e635a74ba17f7416
Bryan Workman <bryan@bryans-mbp.lan>
2021-08-18 08:54:43 -0700

    add starter project

---
+++ GitTest40/GitTest40/Controllers/ModelControllers/ModelControllerPlaceholder.swift
@@ -0,0 +1,8 @@
+//
+//  ModelControllerPlaceholder.swift
+//  GitTest40
+//
+//  Created by Bryan Workman on 8/18/21.
+//
+
+import Foundation
---
+++ GitTest40/GitTest40/Helpers+Extensions/HelpersPlaceholder.swift
@@ -0,0 +1,8 @@
+//
+//  HelpersPlaceholder.swift
+//  GitTest40
+//
+//  Created by Bryan Workman on 8/18/21.
+//
+
+import Foundation
